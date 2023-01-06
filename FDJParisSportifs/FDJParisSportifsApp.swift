4a6b69f75ebff207b0920e6101760a27aaed8e44
Cédrik SARAIVA <cedriksaraiva@gmail.com>
2023-01-06 13:58:25 +0100

    Initial Commit

---
+++ FDJParisSportifs/FDJParisSportifsApp.swift
@@ -0,0 +1,17 @@
+//
+//  FDJParisSportifsApp.swift
+//  FDJParisSportifs
+//
+//  Created by Cedrik on 06/01/2023.
+//
+
+import SwiftUI
+
+@main
+struct FDJParisSportifsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
